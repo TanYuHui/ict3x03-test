a68e23c1c1a3d933f0ec994d858c58d15bc10bbd
Hui <73220938+TanYuHui@users.noreply.github.com>
2022-11-17 18:00:40 +0800

    commit

--- pastel_de_luna/app/urls.py
+++ pastel_de_luna/app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.views.generic import RedirectView
 
-from views import home
+from views import home, displaySearchResult
 
 
 from django.conf.urls.static import static
@@ -12,5 +12,6 @@ urlpatterns = [
      # path(route, view, kwargs=None, name=None) << syntax
      path("", home, name="home"),
      path("home", home, name="home"),
+     path("displaySearchResult", displaySearchResult, name="displaySearchResult"),
 
 ]
--- pastel_de_luna/app/views.py
+++ pastel_de_luna/app/views.py
@@ -1,10 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Users
 
 
 # Create your views here.
 def home(request):
     # inner join with id where user id =1
-    obj = request.POST.get('searchterm')
-    print(obj)
-    return render(request, "home.html")
+    if request.method == 'POST':
+        obj = request.POST.get('searchterm')
+        print(obj)
+        context = {
+            'search': obj,
+        }
+        print(context)
+        return render(request, "displaySearchResult.html", context)
+    else:
+        return render(request, "home.html")
+
+
+def displaySearchResult(request):
+    return redirect("/")
