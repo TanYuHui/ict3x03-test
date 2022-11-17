c3387e54bda13f66f90cb855503ef8d2bfb228ed
Hui <73220938+TanYuHui@users.noreply.github.com>
2022-11-17 18:21:47 +0800

    update views

--- webapp2/app/views.py
+++ webapp2/app/views.py
@@ -1,21 +1,27 @@
 from django.shortcuts import render, redirect
-from .models import Users
+
+from django.views.decorators.debug import sensitive_post_parameters
+from django.utils.html import escape
 
 
+
+@sensitive_post_parameters()
 # Create your views here.
 def home(request):
-    # inner join with id where user id =1
     if request.method == 'POST':
-        obj = request.POST.get('searchterm')
-        print(obj)
-        context = {
-            'search': obj,
-        }
-        print(context)
+        obj = escape(request.POST.get('searchterm'))
+        if obj == "":
+            return render(request, "home.html")
+        else:
+            print(obj)
+            context = {
+                'search': obj,
+            }
+            print(context)
         return render(request, "displaySearchResult.html", context)
     else:
         return render(request, "home.html")
 
 
-def displaySearchResult(request):
+def displaySearchResult():
     return redirect("/")
